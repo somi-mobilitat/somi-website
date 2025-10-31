c98da7d849849b1deeb4d41084b02fc2941bdeae
OriolCampsPerez <oriol.camps-perez@univ-tlse3.fr>
2025-10-31 13:37:54 +0100

    Hero

---
+++ src/components/hero/Hero.data.js
@@ -0,0 +1,31 @@
+export const heroContent = {
+    badge: 'Trajectes compartits organitzats per comunitats',
+    slogan: {
+        main: 'Comunitat en',
+        highlight: 'moviment',
+    },
+    description: `
+    <b class='text-gray-900 dark:text-white'>Somi</b> et connecta amb conductors i passatgers en
+    <b class='text-somi-600'> comunitats locals </b> de confiança, facilitant
+    <b class='text-somi-600'> compartir trajectes habituals </b> de manera
+    <b class='text-somi-600'> eficient, segura i sostenible</b>.
+  `,
+    cta: 'Com Funciona',
+    features: [
+        {
+            icon: 'Users',
+            title: 'Comunitats',
+            description: 'Uneix-te a xarxes de confiança',
+        },
+        {
+            icon: 'MapPin',
+            title: 'Trajectes',
+            description: 'Crea o uneix-te a trajectes',
+        },
+        {
+            icon: 'Calendar',
+            title: 'Calendari',
+            description: 'Planifica els teus desplaçaments',
+        },
+    ],
+};
